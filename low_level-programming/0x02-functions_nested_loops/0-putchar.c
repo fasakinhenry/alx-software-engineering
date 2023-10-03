b0b3bc5d72222c4b943ad27bdd582bb841eaf920
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-03 22:36:09 +0100

    [feature] Added putchar folder with code

---
+++ low_level-programming/0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: program that prints _putchar, followed by a new line.
+ * The program should return 0
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	/* The putchar accepts only character so we have to use multiple*/
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ low_level-programming/0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* The printf function by Julien Barbier ALX*/
+int _putchar(char c);
+
+#endif
