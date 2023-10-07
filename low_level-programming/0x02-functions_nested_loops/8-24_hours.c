7e38059856e9dc7e121d0fbdd1e424cb4311e2da
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-07 12:16:43 +0100

    [feature]: Add a function for task 8
    prints all the min of the day of Jack Bauer

---
+++ low_level-programming/0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every min of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ *
+ * Return: On success, 0
+ * on error, _1 is returned and the errno is set apporpriately
+*/
+
+void jack_bauer(void)
+{
+	int hrs, mins;
+
+	for(hrs = 0; hrs < 24; hrs++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
