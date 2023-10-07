77e7a6347c5985eeb16d87fb6d07417c2a025ce2
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-07 15:22:15 +0100

    [feature]: Add the prototype for 11-print_to_98 and 100-times_table
    Define prototype for the two function in the header file

--- low_level-programming/0x02-functions_nested_loops/main.h
+++ low_level-programming/0x02-functions_nested_loops/main.h
@@ -30,4 +30,12 @@ void jack_bauer(void);
 /* prints the 9 times table, starting with 0 */
 void times_table(void);
 
+/* function that adds two integers and returns the result */
+int add(int, int);
+
+/* prints all natural numbers from n to 98 and new line */
+void print_to_98(int n);
+
+void print_times_table(int n);
+
 #endif
