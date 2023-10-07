f615aac31c963260ee47490de236c28f194fea1e
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-07 13:39:32 +0100

    [feature]: Add the 10-add.c, 10-main.c and 10-add exe file

---
+++ low_level-programming/0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: first parameter
+ * @b: Second parameter to be added
+ *
+ * Return: addition of a and b
+ * on error, -1 is returned and errno is set appropriately
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
